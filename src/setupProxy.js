4cb905ae4ff533b1e5758a51cb58f545586557da
Raul <raulgomes88@gmail.com>
2020-04-08 13:22:20 -0300

    Adiciona variável de ambiente para a URL da API

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,10 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
 
-    app.use(proxy("/api", { target: "http://localhost:5000/" }));
+    const api = process.env.REACT_APP_API_URL !== undefined ?
+                process.env.REACT_APP_API_URL :
+                "http://localhost:5000/";
+
+    app.use(proxy("/api", { target: api }));
 
 };
